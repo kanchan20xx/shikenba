4b91d16aff1ff883c047aecc2a5ad95b0a684382
kanchan20xx <kanchan20xx@gmail.com>
2021-08-30 23:28:37 +0900

    Added automation.py

---
+++ automaton.py
@@ -0,0 +1,87 @@
+# coding at utf-8
+
+EMPTY = "-"
+ALIVE = "*"
+
+
+def count_neighbors(y, x, get):
+    "y,xの周辺のALIVEセルの数を数える"
+    n_ = get(y - 1, x + 0)
+    ne = get(y - 1, x + 1)
+    e_ = get(y + 0, x + 1)
+    se = get(y + 1, x + 1)
+    s_ = get(y + 1, x + 0)
+    sw = get(y + 1, x - 1)
+    w_ = get(y + 0, x - 1)
+    nw = get(y - 1, x - 1)
+    neighbor_states = [n_, ne, e_, se, s_, sw, w_, nw]
+    count = 0
+    for state in neighbor_states:
+        if state == ALIVE:
+            count += 1
+    return count
+
+
+def game_logic(state, neighbors):
+    if state == ALIVE:
+        if neighbors < 2:
+            return EMPTY
+        elif neighbors > 3:
+            return EMPTY
+    else:
+        if neighbors == 3:
+            return ALIVE
+    return state
+
+
+def step_cell(y, x, get, set):
+    "ステップ処理"
+    state = get(y, x)
+    neighbors = count_neighbors(y, x, get)
+    next_state = game_logic(state, neighbors)
+    set(y, x, next_state)
+
+
+def simulate(grid):
+    next_grid = Grid(grid.height, grid.width)
+    for y in range(grid.height):
+        for x in range(grid.width):
+            step_cell(y, x, grid.get, next_grid.set)
+    return next_grid
+
+
+class Grid:
+    def __init__(self, height, width) -> None:
+        self.height = height
+        self.width = width
+        self.rows = []
+        for _ in range(self.height):
+            self.rows.append([EMPTY] * self.width)
+
+    def get(self, y, x):
+        return self.rows[y % self.height][x % self.width]
+
+    def set(self, y, x, state):
+        self.rows[y % self.height][x % self.width] = state
+
+    def __str__(self):
+        result = ""
+        for row in self.rows:
+            test = ""
+            for cell in row:
+                test += cell
+            result += test
+            result += "\n"
+        return result
+
+
+grid = Grid(5, 9)
+grid.set(0, 3, ALIVE)
+grid.set(1, 4, ALIVE)
+grid.set(2, 2, ALIVE)
+grid.set(2, 3, ALIVE)
+grid.set(2, 4, ALIVE)
+print(grid)
+
+print(simulate(grid))
+print(simulate(simulate(grid)))
